4040ba4bea1db764bd9ebdad7f704eb054603040
Kartik Ohri <kartikohri13@gmail.com>
2020-08-24 16:15:19 +0530

    Remove AudioFile from public API
    The public KTaglib should act only as a wrapper to Taglib. The AudioFile
    is instead moved to the sample. The project is under Apache License so
    if needed the user may copy the file to their project directly without
    any issues.

--- lib/src/main/java/com/simplecityapps/ktaglib/KTagLib.kt
+++ lib/src/main/java/com/simplecityapps/ktaglib/KTagLib.kt
@@ -1,7 +1,6 @@
 package com.simplecityapps.ktaglib
 
-import android.util.Log
-import java.util.HashMap
+import java.util.*
 
 object KTagLib {
 
@@ -35,36 +34,6 @@ object KTagLib {
      */
     @JvmStatic
     external fun writeMetadata(fileDescriptor: Int, properties : HashMap<String, String>) : Boolean
-
-    /**
-     * Returns an [AudioFile] with fields populated by invoking [getMetadata] on [fileDescriptor].
-     *
-     * @param fileDescriptor associated with the file whose properties are to be retrieved
-     * @param filePath path to the file
-     * @param fileName name of the file
-     * @return AudioFile with fields populated from properties of the desired file
-     */
-    @JvmStatic
-    fun getAudioFile(fileDescriptor: Int, filePath: String, fileName: String) : AudioFile {
-        val properties = getMetadata(fileDescriptor)
-        Log.d("Metadata Properties", properties.toString())
-        return AudioFile(
-            filePath,
-            properties["SIZE"]?.toLong() ?: 0,
-            properties["LAST_MODIFIED"]?.toLong() ?: 0,
-            properties["TITLE"],
-            properties["ALBUMARTIST"],
-            properties["ARTIST"],
-            properties["ALBUM"],
-            properties["TRACK"]?.toInt(),
-            properties["TRACKTOTAL"]?.toInt(),
-            properties["DISC"]?.toInt(),
-            properties["DISCTOTAL"]?.toInt(),
-            properties["DURATION"]?.toInt(),
-            properties["DATE"],
-            properties["GENRE"]
-        )
-    }
 
     /**
      * Returns a [ByteArray] representing the artwork for the file located at File Descriptor
